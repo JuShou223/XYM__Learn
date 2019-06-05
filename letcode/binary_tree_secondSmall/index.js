52b5a3f69d5cf98b23c0db28338cf31c2e3df49f
unknown <1318489831@outlook.com>
2019-06-05 19:10:40 +0800

    vue-iview

---
+++ letcode/binary_tree_secondSmall/index.js
@@ -0,0 +1,14 @@
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findSecondMinimumValue = function(root) {
+    for(let i = 0; i<root.length; i++){
+        let node = new TreeNode(root[i])      
+    }    
+};
